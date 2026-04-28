00a33d19c089f1e90f9275aa6ff90d2f86864b7c
subhendu989 <subhendu.bachar@gmail.com>
2026-04-28 15:18:03 +0600

    Initial commit

---
+++ giftVoucher_test.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+// config
+export const options = {
+  stages: [
+    { duration: '50s', target: 100 },   // Increase to 100 users  (Ramp-up)
+    { duration: '250s', target: 500 },  // Up to 500 users
+    { duration: '500s', target: 1000 }, // Load test up to 1000 users
+    { duration: '1m', target: 0 },      // Slowly reducing the user
+  ],
+  
+  // Performance requirments (SLA)
+  thresholds: {
+    'http_req_duration': ['p(95)<2500'], //95% request < 2.5 sec
+    'http_req_failed': ['rate<0.01'],    //error rate < 1%
+  },
+};
+
+export default function () {
+  const url = 'https://giftify.sparktro.cloud'; 
+  
+  // Request Send
+  const res = http.get(url);
+
+  // result check
+  check(res, {
+    'Status is 200': (r) => r.status === 200,
+    'Page Loaded': (r) => r.body.length > 0,
+    'Protocol is HTTPS': (r) => r.url.startsWith('https'),
+  });
+
+  
+  sleep(1);
+}
---
+++ report.js
@@ -0,0 +1,43 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+// Generate HTML report
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+  // step load configaration
+  stages: [
+    { duration: '50s', target: 100 },   // প্রথম ৫০ সেকেন্ডে ১০০ ইউজার পর্যন্ত বাড়বে
+    { duration: '250s', target: 500 },  // পরবর্তী ২৫০ সেকেন্ডে ৫০০ ইউজার হবে
+    { duration: '500s', target: 1000 }, // শেষ ৫০০ সেকেন্ডে ১০০০ ইউজার হবে
+    { duration: '1m', target: 0 },      // শেষে ১ মিনিটে ইউজার সংখ্যা ০ তে নেমে আসবে
+  ],
+
+  // Performance standards (SLA)
+  thresholds: {
+    'http_req_duration': ['p(95)<2500'], // ৯৫% রিকোয়েস্ট ২.৫ সেকেন্ডের নিচে হতে হবে
+    'http_req_failed': ['rate<0.01'],    // এরর ১% এর কম হতে হবে
+  },
+};
+
+export default function () {
+  //demo site URL
+  const url = 'https://giftify.sparktro.cloud/'; 
+  
+  const res = http.get(url);
+
+  // Checking whether the request is successful or not
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'page loaded successfully': (r) => r.body.length > 0,
+  });
+
+  // 1 second pause (realistic user behavior)
+  sleep(1);
+}
+
+// This function will create a report file named "giftify_performance_report.html" after the test is complete.
+export function handleSummary(data) {
+  return {
+    "giftify_performance_report.html": htmlReport(data),
+  };
+}
